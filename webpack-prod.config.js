cd7921ba9a2554d5d9d4f0a4bc4f9f5dcd86e3a4
tuoitrevohoc <tuoitrevohoc@me.com>
2018-10-12 19:00:37 +0800

    add build optimization

--- webpack-prod.config.js
+++ webpack-prod.config.js
@@ -1,4 +1,7 @@
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+
 const devConfig = require('./webpack.config');
 
 const config = {
@@ -18,6 +21,16 @@ const config = {
     ],
   },
   devtool: 'source-map',
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({
+        cache: true,
+        parallel: true,
+        sourceMap: true, // set to true if you want JS source maps
+      }),
+      new OptimizeCSSAssetsPlugin({}),
+    ],
+  },
   plugins: [
     ...devConfig.plugins,
     new MiniCssExtractPlugin({
